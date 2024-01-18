9aa45702ebfbacf2a26adcd718d344650a42244a
merondun <heritabilities@gmail.com>
2024-01-18 01:40:49 +0100

    update final revision

--- admixture/4.ABBA-BABA.sh
+++ admixture/4.ABBA-BABA.sh
@@ -2,16 +2,15 @@
 
 #SBATCH --get-user-env
 #SBATCH --mail-user=merondun@bio.lmu.de
-#SBATCH --clusters=serial
-#SBATCH --partition=serial_std
-#SBATCH --mem=10000mb
+#SBATCH --clusters=biohpc_gen
+#SBATCH --partition=biohpc_gen_normal
 #SBATCH --cpus-per-task=5
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 
 # Calculate ABBA-BABA for each chromosome and with canorus grey or canorus rufous as the target
 CHR=$1
 GROUP=$2
 
 # Using Simon martins' scripts https://github.com/simonhmartin/genomics_general and the tutorial found here : https://github.com/simonhmartin/tutorials/tree/master/ABBA_BABA_whole_genome
-~/modules/genomics_general/freq.py -t 5 -g allsites/${CHR}.geno.gz -p OG -p OH -p ${GROUP} -p CP --popsFile populations/${GROUP}_ABBA.pop --target derived -o ABBA/${CHR}-${GROUP}.derFreq.tsv.gz
 
+~/modules/genomics_general/freq.py -t 5 -g ../input/${CHR}.geno.gz -p OG -p OH -p CG -p CH -p CP --popsFile ${GROUP}.pop --target derived -o ABBA/${CHR}-${GROUP}.derFreq.tsv.gz
--- admixture/5.Plot_ABBA-BABA.R
+++ admixture/5.Plot_ABBA-BABA.R
@@ -1,9 +1,15 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
 #### GW ABBA-BABA from https://github.com/simonhmartin/tutorials/tree/master/ABBA_BABA_whole_genome
-setwd('/dss/dsslegfs01/pr53da/pr53da-dss-0021/projects/2021__Cuckoo_Resequencing/vcfs/all_samples-2022_11/plumage/toes/ABBA/')
+setwd('/dss/dsslegfs01/pr53da/pr53da-dss-0021/projects/2021__Cuckoo_Resequencing/vcfs/all_samples-2022_11/plumage/toes/ABBA/2024JAN')
 library(tidyverse)
 library(viridis)
 library(data.table)
 source("~/modules/genomics_general/jackknife.R")
+
+#submit with group e.g. HUN_Parity
+pop = args[1]
 
 D.stat <- function(p1, p2, p3) {
   ABBA <- (1 - p1) * p2 * p3
@@ -12,67 +18,93 @@ D.stat <- function(p1, p2, p3) {
 }
 
 groups = c('CH','CG')
-AvZs = c('W','Z','Autosome')
 res = NULL
 
+freq_table = read_tsv(paste0('ABBA/',pop,'_2024JAN17.txt')) %>% na.omit %>% as.data.frame
+
 for (group in groups){
-  freq_table_full = read.table(paste0(group,'.ABBA.txt'),header=FALSE)
-  names(freq_table_full) = c('scaffold','position','OG','OH',group,'CP')
-  freq_table_full = freq_table_full %>% mutate(AvZ = ifelse(scaffold == 'chr_Z','Z',ifelse(scaffold == 'chr_W','W','Autosome')))
-  
-  for (comp in AvZs) {
-    cat('Working on group : ',group,' for compartment: ',comp,'\n')
-    freq_table = freq_table_full %>% filter(AvZ == comp)
-    
-    P1 = "OG"
-    P2 = "OH"
-    P3 = group
-    
-    # Calculate D statistic
-    D = D.stat(freq_table[,P1], freq_table[,P2], freq_table[,P3])
-    
-    # Divide for jackknifing
-    block_indices <- get.block.indices(block_size=1e6,
-                                       positions=freq_table$position,
-                                       chromosomes=freq_table$scaffold)
-    
-    n_blocks <- length(block_indices)
-    
-    print(paste("Genome divided into", n_blocks, "blocks."))
-    
-    # Jackknife it
-    D_jackknife <- block.jackknife(block_indices=block_indices,
-                                   FUN=D.stat,
-                                   freq_table[,P1], freq_table[,P2], freq_table[,P3])
-    print(paste("D jackknife mean =", round(D_jackknife$mean,4)))
-    
-    D_Z <- D_jackknife$mean / D_jackknife$standard_error
-    print(paste("D Z score = ", round(D_Z,3)))
-    
-    # Merge and bind results
-    r = data.frame(Group = group, AvZ = comp, D = D, Djack = D_jackknife, Z = D_Z)
-    res = rbind(r,res)
-  }
+  cat('Working on group : ',group,'\n')
+
+  P1 = "OG"
+  P2 = "OH"
+  P3 = group
+
+  # Calculate D statistic
+  D = D.stat(freq_table[,P1], freq_table[,P2], freq_table[,P3])
+
+  # Divide for jackknifing
+  block_indices <- get.block.indices(block_size=1e6,
+                                     positions=freq_table$position,
+                                     chromosomes=freq_table$scaffold)
+
+  n_blocks <- length(block_indices)
+
+  print(paste("Genome divided into", n_blocks, "blocks."))
+
+  # Jackknife it
+  D_jackknife <- block.jackknife(block_indices=block_indices,
+                                 FUN=D.stat,
+                                 freq_table[,P1], freq_table[,P2], freq_table[,P3])
+  print(paste("D jackknife mean =", round(D_jackknife$mean,4)))
+
+  D_Z <- D_jackknife$mean / D_jackknife$standard_error
+  print(paste("D Z score = ", round(D_Z,3)))
+
+  # Merge and bind results
+  r = data.frame(Group = group, Djack = D_jackknife, Z = D_Z)
+  res = rbind(r,res)
+
 }
 
-write.table(res,file='~/merondun/cuculus_plumage/admixture/Jackknife_Dstatistics_2024JAN12.txt',quote=F,sep='\t',row.names=F)
-res = read.table('~/merondun/cuculus_plumage/admixture/Jackknife_Dstatistics_2024JAN12.txt',header=TRUE)
+#admixture proportion (f) using P3a and P3b "average proportion of foreign ancestry in any given haploid genome"
+abba = function(p1, p2, p3) (1 - p1) * p2 * p3
+baba = function(p1, p2, p3) p1 * (1 - p2) * p3
+P1 = 'OG'
+P2 = 'OH'
+P3a = 'CG'
+P3b = 'CH'
+
+ABBA_1_2_3a = abba(freq_table[,P1], freq_table[,P2], freq_table[,P3a])
+BABA_1_2_3a = baba(freq_table[,P1], freq_table[,P2], freq_table[,P3a])
+
+ABBA_1_3b_3a = abba(freq_table[,P1], freq_table[,P3b], freq_table[,P3a])
+BABA_1_3b_3a = baba(freq_table[,P1], freq_table[,P3b], freq_table[,P3a])
+
+f = (sum(ABBA_1_2_3a) - sum(BABA_1_2_3a))/
+  (sum(ABBA_1_3b_3a) - sum(BABA_1_3b_3a))
+f
+
+#bind d stats and f
+res$f = f
+res$pop = pop
+
+write.table(res,file=paste0('Jackknife_Dstatistics_',pop,'2024JAN12.txt'),quote=F,sep='\t',row.names=F)
+
+
+#### Plot ABBA-BABA D-statistics 
+setwd('/dss/dsslegfs01/pr53da/pr53da-dss-0021/projects/2021__Cuckoo_Resequencing/vcfs/all_samples-2022_11/plumage/toes/ABBA/2024JAN')
+library(tidyverse)
+library(viridis)
+library(data.table)
+source("~/modules/genomics_general/jackknife.R")
+
+res = read.table('~/merondun/cuculus_plumage/admixture/Jackknife_Dstatistics_2024JAN17.txt',header=TRUE)
 
 ap = res %>% 
-  filter(AvZ == 'Autosome') %>% 
-  ggplot(aes(x=AvZ,y=Djack.mean,ymin=Djack.mean-Djack.standard_deviation,
+  filter(pop == 'HUN_Parity') %>% 
+  ggplot(aes(x=Group,y=Djack.mean,ymin=Djack.mean-Djack.standard_deviation,
              ymax=Djack.mean+Djack.standard_deviation,
              col=Group,label=paste0(signif(Djack.mean,3),'\n (Z = ',signif(Z,3),')')))+
   geom_point(position=position_dodge(width=0.75))+
-  geom_text(aes(y=Djack.mean+Djack.standard_deviation + 0.06),position=position_dodge(width=0.75),size=4)+
+  geom_label(aes(y=Djack.mean+Djack.standard_deviation + 0.03),position=position_dodge(width=0.75),size=1.75)+
   geom_errorbar(width=0.25,position=position_dodge(width=0.75))+
   xlab('')+ylab('D-Statistic')+
   scale_color_manual(values=c('black','orange'))+
   geom_hline(yintercept=0,lty=2)+
+  ylim(c(-0.06,0.14))+
   theme_bw()
 ap
 
-pdf('~/merondun/cuculus_plumage/admixture/Dstatistics_2024JAN12.pdf',height=3,width=2.5)
+pdf('~/merondun/cuculus_plumage/admixture/Dstatistics_2024JAN17.pdf',height=2,width=2.5)
 ap
 dev.off()
-
